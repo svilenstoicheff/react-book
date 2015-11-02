6ffbd1dd4be0d584779da80af0a049258e428276
Svilen Stoicheff <svilen.stoicheff@orbitz.com>
2015-11-02 14:15:35 -0600

    adding table rows and cells

--- js/books.js
+++ js/books.js
@@ -17,15 +17,31 @@ var headers = ["Book", "Author", "Language", "Published", "Sales"],
 	//component
 	var Excel = React.createClass({
 		displayName: 'Excel',
+		getInitialState: function(){
+			return {data: this.props.initialData};
+		}, 
+		propTypes: {
+					headers: React.PropTypes.arrayOf(React.PropTypes.string),
+					initialData: React.PropTypes.arrayOf(
+									React.PropTypes.arrayOf(React.PropTypes.string)
+								 )  
+		}, 
 		render: function(){
 			return (React.DOM.table(null, 
-				React.DOM.thead(null, 
+					React.DOM.thead(null, 
 					React.DOM.tr(null, 
-						this.props.headers.map(function(title){
-							return React.DOM.th(null, title);
+						this.props.headers.map(function(title, idx){
+							return React.DOM.th({key: idx}, title);
 						})
 						)
-					)
+					),
+					React.DOM.tbody(null, this.state.data.map(function(row, idx){
+						return (React.DOM.tr({key: idx}, row.map(function(cell, idx){
+							return React.DOM.td({key: idx}, cell);
+							
+						})));
+					})) 
+					
 				)
 			);
 		}
